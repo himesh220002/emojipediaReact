b01d50042c55d1d8504675275e5f3330532ef5c0
Himesh <35775892+himesh220002@users.noreply.github.com>
2023-07-06 23:44:53 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Entry from "./entry";
+import emojipedia from "../emojipedia";
+
+function createEmoji(emojipedia) {
+  return (
+    <Entry
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      description={emojipedia.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
+    </div>
+  );
+}
+
+export default App;
